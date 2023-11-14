e3e231991e4990ef9ddc47261e550284dfb11211
Kristian Bolarić <krbolaric@vsite.hr>
2023-11-14 21:26:02 +0100

    Add project

---
+++ PIN3Lab/PIN3Lab/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PIN3Lab.Models;
+
+namespace PIN3Lab.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PIN3Lab.Models.Car> Car { get; set; } = default!;
+    }
+}
